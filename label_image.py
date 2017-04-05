e63d93706ad011368f83ff31b728a8b67a9923a8
Richard Szczerba <richardszczerba@gmail.com>
2017-04-05 14:18:21 +0100

    init commit

---
+++ label_image.py
@@ -0,0 +1,30 @@
+# import modelues in this line, each seperated by a comma
+# name tenserflow as tf, so it can be used by calling tf.xxx
+import tensorflow as tf, sys
+
+# when running this program, input the location of the image to be classified as an arguemnt.
+image_path = sys.argv[1]
+
+# Gfile -
+# FastGFile -
+# 'rb' -
+# .read() -
+
+image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+label_lines = [line.rstrip() for line in tf.gfile.GFile("retrained_labels.txt")]
+
+with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+with tf.Session() as sess:
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    
+    predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    
+    for node_id in top_k:
+        human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        print('%s (score = %.5f)' % (human_string, score))
